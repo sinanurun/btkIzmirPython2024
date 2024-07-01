b859c6525b850274de4a7c884028d237c28b0e90
Sinan URUN <sinanurun24@gmail.com>
2024-07-01 12:13:34 +0300

    Create ders02.py

---
+++ gun01/ders02.py
@@ -0,0 +1,17 @@
+"""
+integer 85
+float 8.5
+string "Hello World!" 'merhaba' "" " btk akademi"" " '' ' pyton' ''
+boolean True False
+"""
+# aşağıda integer veri tipinde veriler ekrana yazdırılıyor
+print(85)
+print(55)
+# pythonda integer yani tamsayılar int ile belirtilir int() fonk ile tanımlanır
+print(type(85))
+print(type(74))
+
+# aşağıdaki ondalıklı sayılar float olarak tanımlanır ve float() fonksiyonu ile belitilir
+print(85.0)
+print(66.74)
+print(type(75.8))
