6dda479acdd9730bad075380a60a397eb7332707
Sinan URUN <sinanurun24@gmail.com>
2024-07-01 14:05:16 +0300

    Create ders07.py

---
+++ gun01/ders07.py
@@ -0,0 +1,10 @@
+# matematiksel işlemler ve veri tipleri arasındaki farklılıklar
+print(35 * 87) # * operatörü sayılarda çarpma yapar
+print(35 * 87.7)
+print(35 * 87.0)
+# / operatörü sayılarda bölme işlemi yapar sonucu herzaman float olur
+print(35 / 5)
+print(35 / 5.0)
+print(35 + 5.0)
+print("35" + "90") # + operatörü stringlerde birleştirme yapar
+print("kadirkadir" * 2) # * operatörü stringlerde çoğaltma işlemi için kullanılır
