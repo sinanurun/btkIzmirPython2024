612787afe73f15a4de45d60b48520a8ca6b6edb8
Sinan URUN <sinanurun24@gmail.com>
2024-07-02 14:38:27 +0300

    df

---
+++ gun02/ders24.py
@@ -0,0 +1,6 @@
+cevap = input("Pizza İster Misiniz?")
+while cevap == "evet":
+    print("afiyet olsun")
+    cevap = input("bir dilim Pizza daha ister misiniz ?")
+    if cevap == "hayır":
+        print("afiyet olsun bizi tercih ettiğin için teşekkürler")
