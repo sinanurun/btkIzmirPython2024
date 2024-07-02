73453e508b029115a0982c6a2d333d243d62875e
Sinan URUN <sinanurun24@gmail.com>
2024-07-02 13:42:20 +0300

    xcz

---
+++ gun02/ders16.py
@@ -0,0 +1,10 @@
+# for döngüsü kaç kere çalışacak olduğu belli olan döngüdür
+print(list(range(5)))
+for dd in range(0,5):
+    print(dd, "merhaba")
+isim = "zuhal"
+for harf in isim:
+    print(harf)
+
+for dilim in range(1,6,2):
+    print(dilim,". pizzanız")
