2d98a59295e6584eb5efa8f6ff9d9a3b2ffa6885
Sinan URUN <sinanurun24@gmail.com>
2024-07-02 09:51:25 +0300

    ders3

---
+++ gun02/ders02.py
@@ -0,0 +1,3 @@
+cevap = bool(input("Kursa katılım tam mı ? - tamsa evet yoksa enter"))
+if cevap:
+    print("Kurstan sertifika alabilirsin")
---
+++ gun02/ders03.py
@@ -0,0 +1,12 @@
+#koşullu ifadeler için if yapısı kullanılır
+# koşullu ifadeler yani if True değerini arar
+
+ad = input("adınızı giriniz")
+if ad == "umut":
+    print("adaşız seninle")
+
+if not(ad == "umut"):
+    print("adaş değiliz")
+
+if ad != "umut":
+    print("adaş değiliz")
