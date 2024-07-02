55bac6443d5850272e46f8c5b7a11afa07a54ed3
Sinan URUN <sinanurun24@gmail.com>
2024-07-02 10:42:06 +0300

    hj

---
+++ gun02/ders07.py
@@ -0,0 +1,7 @@
+#yeme sipariş uygulaması
+yemek = input("Yemek bilgisi giriniz:")
+icecek = input("Icecek bilgisi giriniz:")
+if yemek=="pide" and icecek =="ayran":
+    print("sipariş doğru")
+else:
+    print("sipariş yanlış")
---
+++ gun02/ders072.py
@@ -0,0 +1,10 @@
+#yeme sipariş uygulaması
+yemek = input("Yemek bilgisi giriniz:")
+if yemek=="pide":
+    icecek = input("Icecek bilgisi giriniz:")
+    if icecek == "ayran":
+        print("sipariş doğru")
+    else:
+        print("yemek doğru içecek yanlış")
+else:
+    print("sipariş yanlış")
