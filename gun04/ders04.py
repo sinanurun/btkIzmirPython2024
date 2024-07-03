7d3df0442c642443b12d95e8920fb6152b5a5b17
Sinan URUN <sinanurun24@gmail.com>
2024-07-03 15:53:53 +0300

    Update ders04.py

--- gun04/ders04.py
+++ gun04/ders04.py
@@ -20,3 +20,23 @@ while tahminSayisi >= 1:
         taban = tahmin
     tahminSayisi -= 1
     print("kalan hakkınız", tahminSayisi)
+
+#versiyon 2
+# import random
+# rastgele = random.randrange(1,1000)
+# print("tutulan sayı",rastgele)
+# tahminSayisi = 10
+# taban = 0
+# tavan = 1001
+# while tahminSayisi >= 1:
+#     tahmin =random.randrange(taban,tavan)
+#     print(tahmin , end=" ")
+#     if tahmin == rastgele:
+#         print("Tebrikler")
+#         break
+#     elif tahmin > rastgele:
+#         tavan = tahmin
+#     elif tahmin < rastgele:
+#         taban = tahmin
+#     tahminSayisi -= 1
+#     print("kalan hakkınız", tahminSayisi)
