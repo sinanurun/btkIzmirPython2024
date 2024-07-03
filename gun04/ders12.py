e54fe7009068442fcc40307505b348d3c4342acc
Sinan URUN <sinanurun24@gmail.com>
2024-07-03 17:35:06 +0300

    sdfsdf

---
+++ gun04/ders12.py
@@ -0,0 +1,8 @@
+def selamla():
+    isim = input("adınız nedir")
+    print("selamlar",isim)
+    return isim
+
+selamla()
+a = selamla()
+print(a,"selamlandı")
---
+++ gun04/ders13.py
@@ -0,0 +1,7 @@
+def selamla(isim):
+    print("selamlar",isim)
+    return "ayşe"
+
+isim = input("adınız nedir")
+a = selamla(isim)
+print("selamlama işlemi",a)
