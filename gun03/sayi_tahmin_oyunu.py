77a18087ed7c5d5a0a2d9c1f361d228943890555
Sinan URUN <sinanurun24@gmail.com>
2024-07-03 13:45:39 +0300

    Update sayi_tahmin_oyunu.py

---
+++ gun03/sayi_tahmin_oyunu.py
@@ -0,0 +1,38 @@
+#versiyon 1 kullanıcıdan sayı alıp tahmin etme
+# sayi = 35
+# tahmin = int(input("Bir sayı giriniz"))
+# if tahmin == sayi:
+#     print("Tahmin başarılı")
+# else:
+#     print("Tahmin Başarısız")
+
+# versiyon 2 aşağı yukarı
+# sayi = 35
+# tahmin = int(input("Bir sayı giriniz"))
+# if tahmin == sayi:
+#     print("Tahmin başarılı")
+# elif tahmin > sayi:
+#     print("Tahmin Başarısız Keşke küçük bür sayı söyleseydin")
+# elif tahmin < sayi:
+#     print("Tahmin Başarısız Keşke küçük bür sayı söyleseydin")
+
+#versiyon 3
+sayi = 35
+taban = 0
+tavan = 100
+print(f"Tahmin edeceğiniz sayı {taban} - {tavan} aralığındadır")
+tahmin = int(input("Bir sayı giriniz"))
+while True:
+    if taban <= tahmin <= tavan:
+        print(f"Tahmin geçerli aralıkta")
+        break
+    else:
+        print(f"Tahmin aralığı dışında geçersiz tahmin")
+        print(f"Tahmin edeceğiniz sayı {taban} - {tavan} aralığındadır")
+        tahmin = int(input("Bir sayı giriniz"))
+if tahmin == sayi:
+    print("Tahmin başarılı")
+elif tahmin > sayi:
+    print("Tahmin Başarısız Keşke küçük bür sayı söyleseydin")
+elif tahmin < sayi:
+    print("Tahmin Başarısız Keşke küçük bür sayı söyleseydin")
