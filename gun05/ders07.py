c8a052e5ad024604c4dcf638cc623883ad426729
Sinan URUN <sinanurun24@gmail.com>
2024-07-04 15:43:34 +0300

    sdf

---
+++ gun05/ders07.py
@@ -0,0 +1,14 @@
+# oop giriş
+class Ogrenci():
+    pass
+
+ogrenci1 = Ogrenci()
+
+ogrenci1.ad = "kerim"
+ogrenci1.soyad = "karakan"
+print(ogrenci1, ogrenci1.ad, ogrenci1.soyad)
+print(type(ogrenci1))
+ogrenci2 = Ogrenci()
+ogrenci2.ad = "ege"
+ogrenci2.soyad = "altun"
+print(ogrenci2, ogrenci2.ad, ogrenci2.soyad)
