164bdca6c187f507b669f626c2657150573baa65
Sinan URUN <sinanurun24@gmail.com>
2024-07-04 15:48:29 +0300

    Update ders08.py

--- gun05/ders08.py
+++ gun05/ders08.py
@@ -1,1 +1,13 @@
 class Ogrenci():
+    kurs = "BTK Python"
+    fakulte = ""
+
+print(Ogrenci.kurs)
+print(Ogrenci.fakulte)
+# print(Ogrenci.bolum)
+ogr1 = Ogrenci()
+print(ogr1.kurs)
+ogr1.fakulte = "Yazılım"
+print(ogr1.fakulte)
+ogr1.bolum = "muhendis"
+print(ogr1.bolum)
