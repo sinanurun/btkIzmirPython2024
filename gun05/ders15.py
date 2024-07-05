26560d2c13a8586f42608d4e803417cd2f08a8aa
Sinan URUN <sinanurun24@gmail.com>
2024-07-05 14:23:32 +0300

    Update ders15.py

--- gun05/ders15.py
+++ gun05/ders15.py
@@ -1,12 +1,34 @@
 class Fatura():
     baslik = "Halil Pazarlama"
     odeme = True
-    icerik = {}
+
     def __init__(self, m_adi, v_no, tarih):
         self.m_adi = m_adi
         self.v_no = v_no
         self.tarih = tarih
         self.tutar = 0
+        self.icerik = {}
+
+    def urunEkle(self):
+        urun_ad = input("ürün bilgisi giriniz")
+        urun_fiyati = int(input(f"{urun_ad} fiyatını giriniz"))
+        urun_adet = int(input(f"{urun_ad} adet bilgisi"))
+        urun_tutar = urun_adet * urun_fiyati
+        self.tutar += urun_tutar
+        self.icerik[urun_ad] = [urun_ad,urun_adet, urun_fiyati,urun_tutar]
+        print(self.icerik[urun_ad], " ürünü sepete eklendi")
+        return self.tutar
+
+    def urunCikar(self):
+        urun_ad = input("ürün bilgisi giriniz")
+        urun_fiyati = int(input(f"{urun_ad} fiyatını giriniz"))
+        urun_adet = int(input(f"{urun_ad} adet bilgisi"))
+        urun_tutar = urun_adet * urun_fiyati
+        self.tutar += urun_tutar
+        self.icerik[urun_ad] = [urun_ad,urun_adet, urun_fiyati,urun_tutar]
+        print(self.icerik[urun_ad], " ürünü sepete eklendi")
+        return self.tutar
+
 
     def __str__(self):
         return self.m_adi+" adlı müşterinin faturası"
