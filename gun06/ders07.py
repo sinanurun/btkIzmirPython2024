1c91446ac12fbcb164e7f600eb08bd5b43eaea2b
Sinan URUN <sinanurun24@gmail.com>
2024-07-05 17:28:49 +0300

    Update ders07.py

--- gun06/ders07.py
+++ gun06/ders07.py
@@ -10,15 +10,32 @@ import json
 # print(y["age"])
 # print(type(y["age"]))
 
-# a Python object (dict):
+# # a Python object (dict):
+# x = {
+#   "name": "John",
+#   "age": 30,
+#   "city": "New York"
+# }
+#
+# # convert into JSON:
+# y = json.dumps(x)
+#
+# # the result is a JSON string:
+# print(y)
+
+class Ogrenci():
+    # def __init__(self,name, age, city):
+    def __init__(self,kwargs):
+        self.ad = kwargs["name"]
+        self.age = kwargs["age"]
+        self.city = kwargs["city"]
+
+# ogr = Ogrenci('ali', 20, 'New York')
+# print(vars(ogr))
 x = {
   "name": "John",
   "age": 30,
   "city": "New York"
 }
-
-# convert into JSON:
-y = json.dumps(x)
-
-# the result is a JSON string:
-print(y)
+ogr = Ogrenci(x)
+print(vars(ogr))
