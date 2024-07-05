09a4573736c26213d0044d0cbe68861995603da1
Sinan URUN <sinanurun24@gmail.com>
2024-07-05 11:53:42 +0300

    Update ders121.py

--- gun05/ders121.py
+++ gun05/ders121.py
@@ -8,8 +8,23 @@ class Ogrenci():
         self.soyad = soyad
         self.tcno = tc
 
+    def tamAd(self):
+        return self.ad + self.soyad
+
+    def kafHarf(self):
+        return len(self.ad)
+
+    def __str__(self):
+        return f"{self.ad} - {self.soyad} - adında bir öğrenci"
 ogr1 = Ogrenci("kerim","can",123)
+
+print(ogr1)
+
 print(ogr1.bolum)
 print(ogr1.kurs)
 print(vars(ogr1))
+print(ogr1.tamAd())
+harf_sayisi = ogr1.kafHarf()
+print(harf_sayisi)
+# print(Ogrenci.tamAd())
 
