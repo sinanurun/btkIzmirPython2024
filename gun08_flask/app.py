b5fae4ccf6c606fef2c990aa3c5367bc38d657ed
Sinan URUN <sinanurun24@gmail.com>
2024-07-09 14:58:12 +0300

    sdfsdf

--- gun08_flask/app.py
+++ gun08_flask/app.py
@@ -5,7 +5,15 @@ from orm_db import *
 @app.route('/')
 def index():  # put application's code here
     mesaj = "Merhaba Dünya"
-    
+    try:
+        kullanici = User.query.filter(User.name=="esma", User.password=="nur").first()
+        print(kullanici)
+        if kullanici == None:
+            mesaj = "Kullanici bulunamadı"
+        else:
+            mesaj = "hoş geldiniz"+ kullanici.name
+    except:
+        mesaj =" sorgu hatası"
     return render_template('index.html', mesaj = mesaj)
 
 @app.route('/yonetici')
